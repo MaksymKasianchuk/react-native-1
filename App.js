23aa23021b31225841f8be3b28b3979a5c6ee6b4
Maksym <maxthebest1906@gmail.com>
2023-03-22 13:10:47 +0200

    run expo on smartphone done

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!!!</Text>
+      <Text style={styles.header}>Hello world!!! I`m alive!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f5f5',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    fontWeight: '700',
+    fontSize: 22,
   },
 });
